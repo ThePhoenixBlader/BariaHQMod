91a57cda248ac325eded9d297fb5228eaa886764
ThePhoenixBlader <thepheonixblader@gmail.com>
2018-07-23 13:08:03 -0700

    Fixed wipeflatlands

--- src/main/java/me/bariahq/bariahqmod/command/Command_wipeflatlands.java
+++ src/main/java/me/bariahq/bariahqmod/command/Command_wipeflatlands.java
@@ -2,10 +2,14 @@ package me.bariahq.bariahqmod.command;
 
 import me.bariahq.bariahqmod.rank.Rank;
 import me.bariahq.bariahqmod.util.FUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.io.File;
 
 @CommandPermissions(level = Rank.ADMIN, source = SourceType.ONLY_CONSOLE)
 @CommandParameters(description = "Wipe the flatlands map. Requires manual restart after command is used.", usage = "/<command>")
@@ -23,7 +27,16 @@ public class Command_wipeflatlands extends FreedomCommand
         {
             player.kickPlayer("Server is going offline for flatlands wipe, come back in a few minutes.");
         }
+        File folder = new File(Bukkit.getWorldContainer() + "/flatlands");
+        folder.delete();
+        for (Chunk c : plugin.getServer().getWorld("flatlands").getLoadedChunks()){
+            c.unload();
+        }
+        Bukkit.unloadWorld("flatlands", true);
+        Bukkit.getServer().spigot().restart();
 
+
+//after this, BHQM/TFM will regenerate "flatlands" when the server starts and loads BHQM/TFM
         return true;
     }
 }
