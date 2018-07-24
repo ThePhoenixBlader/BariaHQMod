f6c0a7fcf27db3ab25923dc1e9cd71bf7e92e73c
ThePhoenixBlader <thepheonixblader@gmail.com>
2018-07-23 17:38:36 -0700

    discord system to report command

--- src/main/java/me/bariahq/bariahqmod/command/Command_report.java
+++ src/main/java/me/bariahq/bariahqmod/command/Command_report.java
@@ -1,12 +1,18 @@
 package me.bariahq.bariahqmod.command;
 
+import me.bariahq.bariahqmod.config.ConfigEntry;
+import me.bariahq.bariahqmod.discord.Discord;
 import me.bariahq.bariahqmod.rank.Rank;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.TextChannel;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.awt.*;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME, blockHostConsole = true)
 @CommandParameters(description = "Report a player for staff members to see.", usage = "/<command> <player> <reason>")
@@ -46,6 +52,21 @@ public class Command_report extends FreedomCommand
 
         String report = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
         plugin.cm.reportAction(playerSender, player, report);
+        //
+            TextChannel channel = Discord.bot.getTextChannelById(ConfigEntry.DISCORD_REPORTS_CHANNELID.getString());
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("New Report");
+            eb.setAuthor(sender.getName());
+            eb.addBlankField(true);
+            eb.setThumbnail(Discord.bot.getTextChannelById(ConfigEntry.DISCORD_REPORTS_CHANNELID.getString()).getGuild().getIconUrl());
+            eb.addField("Reported User", player.getName(), true);
+            eb.addField("Reason", report, true);
+            eb.addField("Reported Player's World", player.getWorld().getName(), true);
+            eb.addField("Reporter's World", playerSender.getWorld().getName(), true);
+            eb.addField("Reported Player's Coords", player.getLocation().getX() + ", " + player.getLocation().getY() + ", " + player.getLocation().getZ(), true);
+            eb.setColor(Color.red);
+            channel.sendMessage(eb.build()).queue();
+            //feature coming later on to enable or disable this discord report system via config
 
         msg(ChatColor.GREEN + "Thank you, all online staff members have been notified." + ChatColor.RED + "Please note that only the current online admins will see it" + ChatColor.GREEN + ".");
 
